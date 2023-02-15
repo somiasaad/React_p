63c72a5db12ddd377f7289bcc787fb2e20670ab3
somiasaad <somiasaa012@gmail.com>
2023-02-15 18:44:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { NavBar } from './component/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import { Banner } from './component/Banner';
+import { Skills } from './component/Skills';
+import {Contact} from './component/Contact';
+// import Contact from '../Component/Contact';
+import {Projects} from'./component/Projects';
+import {Footer} from "./component/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+   render() {
+     return (
+         <div className="App">
+         
+            <NavBar/>
+            <Banner/>
+            <Skills/>
+            <Projects/>
+            <Contact/>
+            <Footer/>
+        
+         </div>
+     );
+   }
 }
-
 export default App;
